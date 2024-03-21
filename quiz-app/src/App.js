e2e04ccfd082f20b03d2e4d7a065f7593e8155a9
Samuel okoye <samuelokoye92@gmail.com>
2024-03-21 17:55:41 +0100

    initail commit

---
+++ quiz-app/src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerClick = (isCorrect) => {
+    if (isCorrect === true) {
+      alert("This answer is correct");
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(score - 1);
+    }
+  };
+
+  const handleResetButton = () => {
+    setCurrentQuestion(0); // Reset current question to the first one
+    setScore(0); // Reset score to 0
+    setShowScore(false); // Hide score section
+  };
+
+  return (
+    <>
+      <h2 className="title">IQ Quiz Game</h2>
+      <div className="app">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+            <button className="resetButton" onClick={handleResetButton}>
+              Reset
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button
+                  onClick={() => handleAnswerClick(answerOption.isCorrect)}
+                >
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
